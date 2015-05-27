7c613adf63c0fdbcccfcba722342c6ccadda0ab5
mxro <mxro@nowhere.com>
2015-05-27 17:24:46 +1200

    Developing first version of simple-server-monitoring Autocommit Wed May 27 15:24:46 AEST 2015

--- src/main/java/com/appjangle/servermonitoring/ProcessBashInstructions.java
+++ src/main/java/com/appjangle/servermonitoring/ProcessBashInstructions.java
@@ -1,15 +1,47 @@
 package com.appjangle.servermonitoring;
 
+import com.appjangle.servermonitoring.types.ServerMonitoringTypes;
+import de.mxro.process.Spawn;
+import io.nextweb.Link;
+import io.nextweb.ListQuery;
 import io.nextweb.Node;
+import io.nextweb.NodeList;
+import io.nextweb.Query;
+import io.nextweb.Session;
+import java.util.List;
+import org.eclipse.xtext.xbase.lib.InputOutput;
 
 @SuppressWarnings("all")
 public class ProcessBashInstructions {
   private Node node;
   
   public void now() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nType mismatch: cannot convert from Class<String> to String"
-      + "\nUse \'as\' keyword for type casting.");
+    Session _session = this.node.session();
+    ServerMonitoringTypes t = new ServerMonitoringTypes(_session);
+    Link _bashInstruction = t.bashInstruction();
+    ListQuery _selectAll = this.node.selectAll(_bashInstruction);
+    final NodeList bashInstructions = _selectAll.get();
+    List<Node> _nodes = bashInstructions.nodes();
+    for (final Node bashInstruction : _nodes) {
+      {
+        Link _bashScript = t.bashScript();
+        Query _select = bashInstruction.select(_bashScript);
+        Node _get = _select.get();
+        Object _value = _get.value();
+        final String script = ((String) _value);
+        final String[] lines = script.split("\n");
+        for (final String line : lines) {
+          {
+            InputOutput.<String>println(("Run: " + line));
+            String res = Spawn.sh(line);
+            Link _result = t.result();
+            Query _select_1 = bashInstruction.select(_result);
+            Query _setValueSafe = _select_1.setValueSafe(res);
+            _setValueSafe.get();
+          }
+        }
+      }
+    }
   }
   
   public ProcessBashInstructions(final Node node) {
--- src/main/xtend/com/appjangle/servermonitoring/ProcessBashInstructions.xtend
+++ src/main/xtend/com/appjangle/servermonitoring/ProcessBashInstructions.xtend
@@ -1,6 +1,7 @@
 package com.appjangle.servermonitoring
 
 import com.appjangle.servermonitoring.types.ServerMonitoringTypes
+import de.mxro.process.Spawn
 import io.nextweb.Node
 
 class ProcessBashInstructions {
@@ -12,8 +13,17 @@ class ProcessBashInstructions {
     	val bashInstructions = node.selectAll(t.bashInstruction).get();
     	
     	for (bashInstruction : bashInstructions.nodes) {
-    		val String script = (String) bashInstruction.value()
+    		val String script = bashInstruction.select(t.bashScript).get().value() as String
     		
+    		val lines = script.split("\n")
+ 
+    		for (line : lines) {
+    			println("Run: "+line)
+    			var res = Spawn.sh(line);
+    			
+    			bashInstruction.select(t.result).setValueSafe(res).get()
+    			
+    		}
     	}
     	
     }
