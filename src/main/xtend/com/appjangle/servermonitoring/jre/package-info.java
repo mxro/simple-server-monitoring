92066133eaedf2e183dd4d59abdd43d0c3e4489f
mxro <mxro@nowhere.com>
2015-05-27 14:53:40 +1000

    a

---
+++ src/main/xtend/com/appjangle/servermonitoring/jre/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes incompatible with GWT.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+package com.appjangle.servermonitoring.jre;
---
+++ src/main/xtend/com/appjangle/servermonitoring/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Main package for project.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+package com.appjangle.servermonitoring;
