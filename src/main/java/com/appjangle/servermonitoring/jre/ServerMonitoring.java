859781e1d34520326bc1d8f9c7215561853e3ee2
mxro <mxro@nowhere.com>
2015-05-27 17:14:46 +1200

    Developing first version of simple-server-monitoring Autocommit Wed May 27 15:14:46 AEST 2015

--- src/main/java/com/appjangle/servermonitoring/jre/ServerMonitoring.java
+++ src/main/java/com/appjangle/servermonitoring/jre/ServerMonitoring.java
@@ -2,7 +2,11 @@ package com.appjangle.servermonitoring.jre;
 
 @SuppressWarnings("all")
 public class ServerMonitoring {
-  public static Object main() {
-    return null;
+  public static void main(final String[] args) {
+    int _length = args.length;
+    boolean _notEquals = (_length != 2);
+    if (_notEquals) {
+      throw new RuntimeException("Supply two arguments: Node and Secret");
+    }
   }
 }
--- src/main/xtend/com/appjangle/servermonitoring/jre/ServerMonitoring.xtend
+++ src/main/xtend/com/appjangle/servermonitoring/jre/ServerMonitoring.xtend
@@ -2,7 +2,11 @@ package com.appjangle.servermonitoring.jre
 
 class ServerMonitoring {
 	
-	def static main() {
+	def static main(String[] args) {
+		if (args.length != 2) {
+			throw new RuntimeException("Supply two arguments: Node and Secret")
+		}
+		
 		
 	}
 	
