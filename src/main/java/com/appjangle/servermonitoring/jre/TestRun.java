8073c7a0941c75ebdabdbbe8f9a0ba1d0a7f4edb
mxro <mxro@nowhere.com>
2015-05-27 17:29:46 +1200

    Developing first version of simple-server-monitoring Autocommit Wed May 27 15:29:46 AEST 2015

--- src/main/java/com/appjangle/servermonitoring/jre/TestRun.java
+++ src/main/java/com/appjangle/servermonitoring/jre/TestRun.java
@@ -1,5 +1,10 @@
 package com.appjangle.servermonitoring.jre;
 
+import com.appjangle.servermonitoring.jre.ServerMonitoring;
+
 @SuppressWarnings("all")
 public class TestRun {
+  public static void main(final String[] args) {
+    ServerMonitoring.main(new String[] { "", "" });
+  }
 }
