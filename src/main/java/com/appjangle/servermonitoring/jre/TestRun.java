b7b5b11e7150b78ff82c78446eeba7cef1416b7d
mxro <mxro@nowhere.com>
2015-07-02 08:30:00 +1000

    Developing daemon for starting and monitoring Appjangle server Autocommit Thu Jul 02 08:30:00 EST 2015

--- src/main/java/com/appjangle/servermonitoring/jre/TestRun.java
+++ src/main/java/com/appjangle/servermonitoring/jre/TestRun.java
@@ -1,11 +1,14 @@
 package com.appjangle.servermonitoring.jre;
 
+import com.appjangle.servermonitoring.jre.ServerMonitoring;
+
 @SuppressWarnings("all")
 public class TestRun {
   public static void main(final String[] args) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method or field heapSize is undefined for the type TestRun"
-      + "\nThe method or field heapSize is undefined for the type TestRun"
-      + "\nThis expression is not allowed in this context, since it doesn\'t cause any side effects.");
+    ServerMonitoring.main(
+      new String[] { "https://ajadmin.nxweb.de/admin1/admin1/.n/Monitoring_and_Logging/.n/Nodes/.n/minivps1/.n/Instructions", "7zuatt3ma4u3ow29" });
+    Runtime _runtime = Runtime.getRuntime();
+    final long heapSize = _runtime.totalMemory();
+    System.out.println(("Heap Size = " + Long.valueOf(heapSize)));
   }
 }
