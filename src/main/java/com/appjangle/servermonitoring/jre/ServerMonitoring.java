fae12f96610488d2626c354ac5af4a9a78920849
mxro <mxro@nowhere.com>
2017-02-21 07:53:09 +1100

    Fixing Bugs Autocommit Tue Feb 21 07:53:09 AEDT 2017

--- src/main/java/com/appjangle/servermonitoring/jre/ServerMonitoring.java
+++ src/main/java/com/appjangle/servermonitoring/jre/ServerMonitoring.java
@@ -1,15 +1,11 @@
 package com.appjangle.servermonitoring.jre;
 
 import com.appjangle.api.Client;
-import com.appjangle.api.Link;
-import com.appjangle.api.ListQuery;
 import com.appjangle.api.Node;
 import com.appjangle.api.NodeList;
 import com.appjangle.api.jre.ClientsGeneralJre;
 import com.appjangle.servermonitoring.jre.instructions.ProcessBashInstructions;
 import com.appjangle.servermonitoring.types.ServerMonitoringTypes;
-import delight.functional.Success;
-import io.nextweb.promise.DataPromise;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -35,26 +31,21 @@ public class ServerMonitoring {
     final String secret = args[1];
     final Client session = ClientsGeneralJre.createClient();
     final ServerMonitoringTypes t = new ServerMonitoringTypes(session);
-    Link _link = session.link(uri, secret);
-    final Node root = _link.get();
+    final Node root = session.link(uri, secret).get();
     String _uri = root.uri();
     String _plus = ("Loaded instructions: " + _uri);
     InputOutput.<String>println(_plus);
-    Link _instructionGroup = t.instructionGroup();
-    ListQuery _selectAll = root.selectAll(_instructionGroup);
-    final NodeList groups = _selectAll.get();
+    final NodeList groups = root.selectAll(t.instructionGroup()).get();
     List<Node> _nodes = groups.nodes();
     for (final Node group : _nodes) {
       {
         String _uri_1 = group.uri();
         String _plus_1 = ("Processing group: " + _uri_1);
         InputOutput.<String>println(_plus_1);
-        ProcessBashInstructions _processBashInstructions = new ProcessBashInstructions(group);
-        _processBashInstructions.now();
+        new ProcessBashInstructions(group).now();
       }
     }
-    DataPromise<Success> _close = session.close();
-    _close.get();
+    session.close().get();
     timer.cancel();
   }
 }
--- src/main/java/com/appjangle/servermonitoring/jre/TestRun.java
+++ src/main/java/com/appjangle/servermonitoring/jre/TestRun.java
@@ -7,8 +7,7 @@ public class TestRun {
   public static void main(final String[] args) {
     ServerMonitoring.main(
       new String[] { "https://ajadmin.nxweb.de/admin1/admin1/.n/Monitoring_and_Logging/.n/Nodes/.n/minivps1/.n/Instructions", "7zuatt3ma4u3ow29" });
-    Runtime _runtime = Runtime.getRuntime();
-    final long heapSize = _runtime.totalMemory();
+    final long heapSize = Runtime.getRuntime().totalMemory();
     System.out.println(("Heap Size = " + Long.valueOf(heapSize)));
   }
 }
