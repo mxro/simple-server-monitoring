4c4dc640e77fd61057140e30ab14b969a619cd58
mxro <mxro@nowhere.com>
2017-08-31 10:14:01 +1000

    Adding depreaction notice.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simple-server-monitoring
+
+**Deprecation Notice** This project is no longer actively maintained. For updates, please check [ObjectHub](https://objecthub.io) and [Java Delight](https://www.javadelight.org).
+
 A simple tool for Server/System Monitoring Implemented with Xtend and Appjangle 
--- src/main/java/com/appjangle/servermonitoring/jre/instructions/ProcessBashInstructions.java
+++ src/main/java/com/appjangle/servermonitoring/jre/instructions/ProcessBashInstructions.java
@@ -1,12 +1,8 @@
 package com.appjangle.servermonitoring.jre.instructions;
 
 import com.appjangle.api.Client;
-import com.appjangle.api.Link;
-import com.appjangle.api.ListQuery;
 import com.appjangle.api.Node;
 import com.appjangle.api.NodeList;
-import com.appjangle.api.Query;
-import com.appjangle.api.engine.fn.IntegerResult;
 import com.appjangle.servermonitoring.types.ServerMonitoringTypes;
 import de.mxro.process.Spawn;
 import java.util.List;
@@ -18,24 +14,14 @@ public class ProcessBashInstructions {
   public void now() {
     Client _client = this.node.client();
     ServerMonitoringTypes t = new ServerMonitoringTypes(_client);
-    Link _bashInstruction = t.bashInstruction();
-    ListQuery _selectAll = this.node.selectAll(_bashInstruction);
-    final NodeList bashInstructions = _selectAll.get();
+    final NodeList bashInstructions = this.node.selectAll(t.bashInstruction()).get();
     List<Node> _nodes = bashInstructions.nodes();
     for (final Node bashInstruction : _nodes) {
       {
-        Link _bashScript = t.bashScript();
-        Query _select = bashInstruction.select(_bashScript);
-        Node _get = _select.get();
-        Object _value = _get.value();
+        Object _value = bashInstruction.select(t.bashScript()).get().value();
         final String script = ((String) _value);
         final String res = Spawn.sh(script);
-        Link _result = t.result();
-        Query _select_1 = bashInstruction.select(_result);
-        Query _setValueSafe = _select_1.setValueSafe(res);
-        Node _get_1 = _setValueSafe.get();
-        IntegerResult _clearVersions = _get_1.clearVersions(2);
-        _clearVersions.get();
+        bashInstruction.select(t.result()).setValueSafe(res).get().clearVersions(2).get();
       }
     }
   }
